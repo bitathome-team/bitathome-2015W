4b5bde327b0964bb5431a4ad063e768234617e3a
snape3058 <alansnape@outlook.com>
2015-02-28 19:10:34 +0800

    mod: change to udp

--- bitathome_remote_control/script/point.py
+++ bitathome_remote_control/script/point.py
@@ -3,6 +3,7 @@
 import rospy
 from geometry_msgs.msg import Point
 import socket
+import traceback
 
 port = 23333	# server port
 links = 1		# max link
@@ -12,21 +13,22 @@ if __name__ == '__main__':
 	rospy.init_node('netPublisher', anonymous=True)
 	rate = rospy.Rate(30) 
 	rospy.loginfo("Initializing socket...")
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock.bind(('localhost', port))
-	sock.listen(links)
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	sock.bind(('', port))
+	rospy.loginfo("Ready!")
 	try:
-		rospy.loginfo("Waiting for client...")
-		connection,address = sock.accept()
-		rospy.loginfo("Connected!")
 		while not rospy.is_shutdown():
-			buf = connection.recv(1024).split(" ")
+			buf,(r_ip, r_port) = sock.recvfrom(1024)
+			print(buf, r_ip, r_port)
+			buf = buf.split(" ")
 			if buf[0] == "xy":
 				ret = Point()
 				ret.x = float(buf[1])
 				ret.y = float(buf[2])
 				pub.publish(ret)
 			rate.sleep()
-	except:
-		rospy.logerr("Connection closed!")
-		connection.close()
+	except Exception,ex:
+		rospy.logerr("Socket closed!")
+		print(ex)
+		traceback.print_exc()		
+
