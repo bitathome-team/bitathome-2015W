f38459457ec39ab6bba5629c7a1268c4bd4c9eae
Snape3058 <alansnape@outlook.com>
2015-04-18 16:07:50 +0800

    add udp-send used to send strings through udp

---
+++ bitathome_kinect_control/udp-send/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace udp_send
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPAddress serverAddr;
+            IPEndPoint endPoint;
+            serverAddr = IPAddress.Parse("192.168.0.6");
+            endPoint = new IPEndPoint(serverAddr, 23333);
+            while (true)
+            {
+                string str = Console.ReadLine() + " ";
+                Byte[] buf = Encoding.ASCII.GetBytes(str);
+                sock.SendTo(buf, endPoint);
+            }
+        }
+    }
+}
